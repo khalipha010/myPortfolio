c1f94eb7e1c2052dc096f9bea142483a1afd1802
Khalifa Jibreel <khalifajibreel@gmail.com>
2025-02-28 20:53:25 +0100

    Update

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,13 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/', // Correct for root deployment on Vercel
   plugins: [
     react(),
     tailwindcss(),
   ],
-})
+  build: {
+    outDir: 'dist', // Ensure output directory is 'dist'
+    assetsDir: 'assets', // Where static assets are stored
+  },
+});
