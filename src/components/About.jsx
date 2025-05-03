66a730573f26c97861135583035b79500d101d1c
Khalifa_Jibreel <57154132+khalipha010@users.noreply.github.com>
2025-05-03 12:17:48 +0100

    Update About.jsx fix blank screen second time

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,4 @@
-mport React, { useState, useCallback, useMemo } from "react";
+import React, { useState, useCallback, useMemo } from "react";
 import { motion } from "framer-motion";
 import { FaReact, FaJs, FaPython, FaFigma, FaNodeJs, FaGit, FaHtml5, FaCss3, FaBootstrap} from "react-icons/fa";
 import { SiFlutter, SiTailwindcss, SiFirebase, SiPostgresql, SiNextdotjs} from "react-icons/si";
